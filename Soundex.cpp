2f34e4e83cca9a45a069bf20fc869f887620d442
Bharathi1603 <bharathikec013@gmail.com>
2024-07-03 13:48:54 +0530

    Refactor soundex algorithm

--- Soundex.cpp
+++ Soundex.cpp
@@ -13,24 +13,137 @@ char getSoundexCode(char c) {
         default: return '0'; // For A, E, I, O, U, H, W, Y
     }
 }
+bool isFirstTwoLettersSame(const std::string& name)
+{
+    if(name.length() < 2)
+    {
+        return false;
+    }
+    return getSoundexCode(name[0]) == getSoundexCode(name[1]);
+}
 
-std::string generateSoundex(const std::string& name) {
-    if (name.empty()) return "";
-
-    std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0]);
-
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
+bool isTwoLettersWithSameDigit(std::string& name, std::uint8_t index)
+{
+    if((name.length() - index) >= 3)
+    {
+        if(getSoundexCode(name[index]) == getSoundexCode(name[index + 2]))
+        {
+            return true;
         }
     }
+    return false;
+}
 
-    while (soundex.length() < 4) {
-        soundex += '0';
+bool isTwoConsecutiveLettersSame(std::string& name, std::uint8_t index)
+{
+    if((name.length() - index) >= 2)
+    {
+        if(getSoundexCode(name[index]) == getSoundexCode(name[index + 1]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isVowelPresent(char ch) {
+    std::string vowels = "aeiou";
+    for (char vowel : vowels) 
+    {
+        if (tolower(ch) == vowel) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isOtherConsonantPresent(char ch) {
+    std::string otherConsonants = "hwy";
+    for (char otherConsonant : otherConsonants) 
+    {
+        if (tolower(ch) == otherConsonant) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void removeCharacterFromString(std::string& name, const char & charToRemove )
+{
+    std::size_t pos = name.find(charToRemove);
+    if (pos != std::string::npos) 
+    {
+        name.erase(pos, 1);
+    }
+}
+
+void encodeDigit(std::string& soundex, char& currentCode, char& prevCode )
+{
+    soundex += currentCode;
+    prevCode = currentCode;  
+}
+
+void encodeTwoSameDigitsSeperatedByVowel(const std::string& name, std::size_t& startIndex, std::string& soundex, char& prevCode)
+{
+    for(std::size_t j =0; j < 3; j++)
+    {
+        char currentCode = getSoundexCode(name[startIndex + j]);
+        if(currentCode != '0')
+        {
+            encodeDigit(soundex, currentCode, prevCode);
+        }
+    }
+    startIndex +=3;
+}
+
+void encodeDigitWithVowelAndOtherConsonant(std::string& name, std::size_t& index, std::string& soundex, char& prevCode)
+{
+    if(isVowelPresent(name[index + 1]))
+    {
+        encodeTwoSameDigitsSeperatedByVowel(name, index, soundex, prevCode);
+    }
+    else if(isOtherConsonantPresent(name[index + 1]))
+    {
+        removeCharacterFromString(name,index+1);
+    }
+}
+
+std::string generateSoundex(std::string name) {
+    if (name.empty()) 
+    {
+        return "";
     }
 
+    if (isFirstTwoLettersSame(name))
+    {
+        removeCharacterFromString(name, name[1]);
+    }
+    
+    std::string soundex(1, toupper(name[0]));
+    char prevCode = getSoundexCode(name[0]);
+    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) 
+    {
+        if(isTwoConsecutiveLettersSame(name, i))
+        {
+           removeCharacterFromString(name, i);
+           continue; 
+        }
+        
+        char currentCode = getSoundexCode(name[i]);
+        if (isTwoLettersWithSameDigit(name, i))
+        {
+            encodeDigitWithVowelAndOtherConsonant(name, i, soundex, prevCode);
+        }
+        else if(currentCode == '0')
+        {
+            continue;
+        }
+        else
+        {
+            encodeDigit(soundex, currentCode, prevCode);
+        }
+    }
+    soundex.resize(4, '0');
+    
     return soundex;
 }
