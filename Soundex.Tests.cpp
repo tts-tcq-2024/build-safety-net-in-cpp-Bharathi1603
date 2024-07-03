723daa3175ad4eb11521b86aca3d85b91f808bd5
Bharathi1603 <bharathikec013@gmail.com>
2024-07-03 16:05:46 +0530

    Add tests in Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,12 +1,19 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-
 TEST(SoundexTest, HandlesEmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
 
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
+}
+
+TEST(SoundexTest, HandleSameRepresentation) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+}
+
+TEST(SoundexTest, HandleSameRepresentation) {
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
 }
 
