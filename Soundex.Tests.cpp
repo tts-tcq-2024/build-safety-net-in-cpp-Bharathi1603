bf243529932578dd9c0cab530169fd2e62055acb
Bharathi1603 <bharathikec013@gmail.com>
2024-07-03 16:12:32 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -9,11 +9,11 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
-TEST(SoundexTest, HandleSameRepresentation) {
+TEST(SoundexTest, RobertRetursR163) {
     EXPECT_EQ(generateSoundex("Robert"), "R163");
 }
 
-TEST(SoundexTest, HandleSameRepresentation) {
+TEST(SoundexTest, RupertRetursR163) {
     EXPECT_EQ(generateSoundex("Rupert"), "R163");
 }
 
